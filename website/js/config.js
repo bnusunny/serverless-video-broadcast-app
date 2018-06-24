5d3f24884022e1efd44e332e73f59efaa15728a2
Harold Sun <bnusunny@gmail.com>
2018-06-24 10:14:21 +0800

    update APIGateway base URL

--- website/js/config.js
+++ website/js/config.js
@@ -3,5 +3,5 @@ var configConstants = {
         domain: 'harold-mb.auth0.com',
         clientId: 'IUz2ElaCdyntX-gwmLu0UK12XPMB3pLW'
     },
-    apiBaseUrl: 'https://0z8e0toi9c.execute-api.us-east-1.amazonaws.com/dev'
+    apiBaseUrl: 'https://ck0y5nvhok.execute-api.us-east-1.amazonaws.com/dev'
 };
